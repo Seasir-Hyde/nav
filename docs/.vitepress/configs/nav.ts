6744af3811f726185b403f96eab8f05faed11460
小逸 <2429016980@qq.com>
2025-02-18 19:14:45 +0800

    feat(auth): 下架旧版本1.5.5版本桌面客户端

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -13,7 +13,6 @@ export const nav: DefaultTheme.Config['nav'] = [
     text: '💻桌面客户端',
     items: [
       { text: '🖥️64位 win10以上（右键复制链接）', link: 'https://client.salesmartly.com/app/desktop/SaleSmartly-windows.zip' },
-      { text: '🖥️32位 win10以下（右键复制链接）', link: 'https://client.salesmartly.com/app/update/SaleSmartly-1.5.5-ia32.exe' },
     ]
   },
   {
